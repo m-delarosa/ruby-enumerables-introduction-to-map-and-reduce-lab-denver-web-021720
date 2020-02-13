4c66a6c4d2895264d4722306d0e72ce110ec14c5
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-13 02:10:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,9 +43,8 @@ def map_to_square(source_array)
   new_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   i = 0
-  starting_point = 0
   result = starting_point
 
 
