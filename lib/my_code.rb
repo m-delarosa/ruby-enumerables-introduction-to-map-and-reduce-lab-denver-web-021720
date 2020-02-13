d89540712bad7d8bef64d81a554fde0abaaf0a02
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-13 02:03:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,4 +44,12 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+  i = 0
+  result = 0
+
+  while i < source_array.length do
+    result += starting_point + source_array + source_array[i]
+    i += 1
+  end
+result
+end
