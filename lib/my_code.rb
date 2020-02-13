525caeaba180927c08cd6c20a5cce1dd65738ceb
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-13 02:14:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,11 +57,10 @@ end
 
 def reduce_to_all_true(source_array)
   i = 0
-  result = nil
 
   while i < + source_array.length do
-    
-
+    return false if source_array[i] == false
     i += 1
   end
+  true
 end
