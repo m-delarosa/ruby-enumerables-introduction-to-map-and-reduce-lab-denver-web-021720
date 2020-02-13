3988e4b3e664101e25c21b63cf371af75242e3f0
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-13 01:51:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
-def map_to_negativize
+def map_to_negativize(source_array)
+  i = 0
+  new_array = []
+
+  while i < source_array.length do
+    new_array << source_array[i] * -1
+    i += 1
+  end
+  new_array
+end
